a9b6f39f7e56d0dbe90771f1a5a2d6b924f21cd7
Tymoteusz Wójcik <wojcik.tymoteusz1999@gmail.com>
2023-06-15 21:28:08 +0200

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let currencyElement = document.querySelector(".js-currency");
+let amountElement = document.querySelector(".js-amount");
+let resultElement = document.querySelector(".js-result");
+let formElement = document.querySelector(".js-form");
+
+formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
+    let rateUSD = 4.07;
+    let rateEUR = 4.45;
+    let rateCHF = 4.56;
+
+    switch (currency) {
+        case "USD": result = amount / rateUSD;
+            break;
+        case "EUR": result = amount / rateEUR;
+            break;
+        case "CHF": result = amount / rateCHF;
+            break;
+    };
+
+    resultElement.textContent = `Za ${amount} PLN dostaniesz ${result.toFixed(2)} ${currency} `;
+});
