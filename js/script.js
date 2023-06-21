ec5938d5af98c2c7fe1b01a731115e2789a2252b
Tymoteusz Wójcik <wojcik.tymoteusz1999@gmail.com>
2023-06-21 21:41:01 +0200

    add code in js to block

--- js/script.js
+++ js/script.js
@@ -1,28 +1,30 @@
-let currencyElement = document.querySelector(".js-currency");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-let formElement = document.querySelector(".js-form");
+{
+    let currencyElement = document.querySelector(".js-currency");
+    let amountElement = document.querySelector(".js-amount");
+    let resultElement = document.querySelector(".js-result");
+    let formElement = document.querySelector(".js-form");
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    let rateUSD = 4.07;
-    let rateEUR = 4.45;
-    let rateCHF = 4.56;
+        let amount = +amountElement.value;
+        let currency = currencyElement.value;
+        let rateUSD = 4.07;
+        let rateEUR = 4.45;
+        let rateCHF = 4.56;
 
-    switch (currency) {
-        case "USD":
-            result = amount / rateUSD;
-            break;
-        case "EUR":
-            result = amount / rateEUR;
-            break;
-        case "CHF":
-            result = amount / rateCHF;
-            break;
-    };
+        switch (currency) {
+            case "USD":
+                result = amount / rateUSD;
+                break;
+            case "EUR":
+                result = amount / rateEUR;
+                break;
+            case "CHF":
+                result = amount / rateCHF;
+                break;
+        };
 
-    resultElement.textContent = `Za ${amount} PLN dostaniesz ${result.toFixed(2)} ${currency} `;
-});
+        resultElement.textContent = `Za ${amount} PLN dostaniesz ${result.toFixed(2)} ${currency} `;
+    });
+}
