5b8b3e6178b4a391f535f433757b5e8d2e7b25ca
Tymoteusz Wójcik <wojcik.tymoteusz1999@gmail.com>
2023-06-21 21:48:37 +0200

    calculateResult function added

--- js/script.js
+++ js/script.js
@@ -4,26 +4,30 @@
     let resultElement = document.querySelector(".js-result");
     let formElement = document.querySelector(".js-form");
 
+    const calculateResult = (currency, amount) => {
+        const rateUSD = 4.07;
+        const rateEUR = 4.45;
+        const rateCHF = 4.56;
+
+        switch (currency) {
+            case "USD":
+                return amount / rateUSD;
+
+            case "EUR":
+                return amount / rateEUR;
+
+            case "CHF":
+                return amount / rateCHF;
+        };
+    }
+
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
 
         let amount = +amountElement.value;
         let currency = currencyElement.value;
-        let rateUSD = 4.07;
-        let rateEUR = 4.45;
-        let rateCHF = 4.56;
 
-        switch (currency) {
-            case "USD":
-                result = amount / rateUSD;
-                break;
-            case "EUR":
-                result = amount / rateEUR;
-                break;
-            case "CHF":
-                result = amount / rateCHF;
-                break;
-        };
+        calculateResult(currency, amount);
 
         resultElement.textContent = `Za ${amount} PLN dostaniesz ${result.toFixed(2)} ${currency} `;
     });
