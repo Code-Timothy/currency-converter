db7e6cbe90e7f3f595959d20aeb7cdda626afd34
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-03-09 21:06:50 +0100

    update calculateResult function

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
 {
-    const formElement = document.querySelector(".js-form");
-    const amountElement = document.querySelector(".js-amount");
-    const resultElement = document.querySelector(".js-result");
-    const selectElement = document.querySelector(".js-select");
-
-    const calculateResult = () => {
+    const calculateResult = (currency, amount) => {
         const eurRate = 4.35;
         const usdRate = 3.95;
         const gbpRate = 5.01;
         const chfRate = 4.48;
-        const amount = amountElement.value;
-        const currency = selectElement.value;
 
         switch (currency) {
             case "EUR":
-                result = amount / eurRate;
-                break;
+                return amount / eurRate;
             case "USD":
-                result = amount / usdRate;
-                break;
+                return amount / usdRate;
             case "GBP":
-                result = amount / gbpRate;
-                break;
+                return amount / gbpRate;
             case "CHF":
-                result = amount / chfRate;
-                break;
+                return amount / chfRate;
         }
-
-        resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
     };
+
+    const formElement = document.querySelector(".js-form");
 
     formElement.addEventListener("submit", (event) => {
         event.preventDefault();
-        calculateResult();
+
+        const amountElement = document.querySelector(".js-amount");
+        const selectElement = document.querySelector(".js-select");
+        const resultElement = document.querySelector(".js-result");
+
+        const amount = amountElement.value;
+        const currency = selectElement.value;
+
+        const result = calculateResult(currency, amount);
+
+        resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
     });
 }
