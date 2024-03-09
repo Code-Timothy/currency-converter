93470c7ff240df0cf64a75c567f2e5ccf0ebf6b0
Tymoteusz Wojcik <timmywojcik@gmail.com>
2024-03-09 20:48:40 +0100

    replace every let to const

--- script.js
+++ script.js
@@ -1,35 +1,37 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let resultElement = document.querySelector(".js-result");
-let selectElement = document.querySelector(".js-select");
+{
+    const formElement = document.querySelector(".js-form");
+    const amountElement = document.querySelector(".js-amount");
+    const resultElement = document.querySelector(".js-result");
+    const selectElement = document.querySelector(".js-select");
 
-let calculateResult = () => {
-    let eurRate = 4.35;
-    let usdRate = 3.95;
-    let gbpRate = 5.01;
-    let chfRate = 4.48;
-    let amount = amountElement.value;
-    let currency = selectElement.value;
+    const calculateResult = () => {
+        const eurRate = 4.35;
+        const usdRate = 3.95;
+        const gbpRate = 5.01;
+        const chfRate = 4.48;
+        const amount = amountElement.value;
+        const currency = selectElement.value;
 
-    switch (currency) {
-        case "EUR":
-            result = amount / eurRate;
-            break;
-        case "USD":
-            result = amount / usdRate;
-            break;
-        case "GBP":
-            result = amount / gbpRate;
-            break;
-        case "CHF":
-            result = amount / chfRate;
-            break;
-    }
+        switch (currency) {
+            case "EUR":
+                result = amount / eurRate;
+                break;
+            case "USD":
+                result = amount / usdRate;
+                break;
+            case "GBP":
+                result = amount / gbpRate;
+                break;
+            case "CHF":
+                result = amount / chfRate;
+                break;
+        }
 
-    resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
-};
+        resultElement.innerText = `${amount} PLN = ${result.toFixed(2)} ${currency}`;
+    };
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    calculateResult();
-});
+    formElement.addEventListener("submit", (event) => {
+        event.preventDefault();
+        calculateResult();
+    });
+}
